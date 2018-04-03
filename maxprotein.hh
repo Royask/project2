e86cf631bcb89e9074c1dfd3043d0c4a01084d49
Royask <roya_us2000@yahoo.com>
2018-04-02 20:01:12 -0700

    maxprotein.hh updated

--- maxprotein.hh
+++ maxprotein.hh
@@ -9,6 +9,7 @@
 
 #pragma once
 
+
 #include <cassert>
 #include <cmath>
 #include <fstream>
@@ -21,48 +22,48 @@
 // One food item in the USDA database.
 class Food {
 private:
-  // Human-readable description of the food, e.g. "all-purpose wheat
-  // flour". Must be non-empty.
-  std::string _description;
+    // Human-readable description of the food, e.g. "all-purpose wheat
+    // flour". Must be non-empty.
+    std::string _description;
 
-  // Human-readable description of the amount of the food in one
-  // sample, e.g. "1 cup". Must be non-empty.
-  std::string _amount;
+    // Human-readable description of the amount of the food in one
+    // sample, e.g. "1 cup". Must be non-empty.
+    std::string _amount;
 
-  // Number of grams in one sample; must be non-negative.
-  int _amount_g;
+    // Number of grams in one sample; must be non-negative.
+    int _amount_g;
 
-  // Energy, in units of kilocalories (commonly called "calories"), in
-  // one sample; must be non-negative.
-  int _kcal;
+    // Energy, in units of kilocalories (commonly called "calories"), in
+    // one sample; must be non-negative.
+    int _kcal;
 
-  // Number of grams of protein in one sample; most be non-negative.
-  int _protein_g;
+    // Number of grams of protein in one sample; most be non-negative.
+    int _protein_g;
 
 public:
-  Food(const std::string& description,
-       const std::string& amount,
-       int amount_g,
-       int kcal,
-       int protein_g)
-    : _description(description),
-      _amount(amount),
-      _amount_g(amount_g),
-      _kcal(kcal),
-      _protein_g(protein_g) {
+    Food(const std::string& description,
+        const std::string& amount,
+        int amount_g,
+        int kcal,
+        int protein_g)
+        : _description(description),
+        _amount(amount),
+        _amount_g(amount_g),
+        _kcal(kcal),
+        _protein_g(protein_g) {
 
-    assert(!description.empty());
-    assert(!amount.empty());
-    assert(amount_g >= 0);
-    assert(kcal >= 0);
-    assert(protein_g >= 0);
-  }
+        assert(!description.empty());
+        assert(!amount.empty());
+        assert(amount_g >= 0);
+        assert(kcal >= 0);
+        assert(protein_g >= 0);
+    }
 
-  const std::string& description() const { return _description; }
-  const std::string& amount() const { return _amount; }
-  int amount_g() const { return _amount_g; }
-  int kcal() const { return _kcal; }
-  int protein_g() const { return _protein_g; }
+    const std::string& description() const { return _description; }
+    const std::string& amount() const { return _amount; }
+    int amount_g() const { return _amount_g; }
+    int kcal() const { return _kcal; }
+    int protein_g() const { return _protein_g; }
 
 };
 
@@ -74,107 +75,109 @@ using FoodVector = std::vector<std::shared_ptr<Food>>;
 // skipped. Returns nullptr on I/O error.
 std::unique_ptr<FoodVector> load_usda_abbrev(const std::string& path) {
 
-  std::unique_ptr<FoodVector> failure(nullptr);
-  
-  std::ifstream f(path);
-  if (!f) {
-    return failure;
-  }
+    std::unique_ptr<FoodVector> failure(nullptr);
 
-  std::unique_ptr<FoodVector> result(new FoodVector);
-
-  for (std::string line; std::getline(f, line); ) {
-
-    std::vector<std::string> fields;
-    std::stringstream ss(line);
-    for (std::string field; std::getline(ss, field, '^'); ) {
-      fields.push_back(field);
+    std::ifstream f(path);
+    if (!f) {
+        return failure;
     }
 
-    if (fields.size() != 53) {
-      return failure;
+    std::unique_ptr<FoodVector> result(new FoodVector);
+
+    for (std::string line; std::getline(f, line); ) {
+
+        std::vector<std::string> fields;
+        std::stringstream ss(line);
+        for (std::string field; std::getline(ss, field, '^'); ) {
+            fields.push_back(field);
+        }
+
+        if (fields.size() != 53) {
+            return failure;
+        }
+
+        std::string descr_field = fields[1],
+            kcal_field = fields[3],
+            protein_g_field = fields[4],
+            amount_g_field = fields[48],
+            amount_field = fields[49];
+
+        auto remove_tildes = [](std::string& output,
+            const std::string& field) {
+            if ((field.size() < 3) ||
+                (field.front() != '~') ||
+                (field.back() != '~')) {
+                return false;
+            }
+            else {
+                output.assign(field.begin() + 1, field.end() - 1);
+                return true;
+            }
+        };
+
+        auto parse_mil = [](int& output, const std::string& field) {
+            std::stringstream ss(field);
+            double floating;
+            ss >> floating;
+            if (!ss) {
+                return false;
+            }
+            else {
+                output = lround(floating);
+                return true;
+            }
+        };
+
+        std::string description, amount;
+        int amount_g, kcal, protein_g;
+        if (remove_tildes(description, descr_field) &&
+            remove_tildes(amount, amount_field) &&
+            parse_mil(amount_g, amount_g_field) &&
+            parse_mil(kcal, kcal_field) &&
+            parse_mil(protein_g, protein_g_field)) {
+            result->push_back(std::shared_ptr<Food>(new Food(description,
+                amount,
+                amount_g,
+                kcal,
+                protein_g)));
+        }
     }
-    
-    std::string descr_field = fields[1],
-                kcal_field = fields[3],
-                protein_g_field = fields[4],
-                amount_g_field = fields[48],
-                amount_field = fields[49];
 
-    auto remove_tildes = [](std::string& output,
-			    const std::string& field) {
-      if ((field.size() < 3) ||
-	  (field.front() != '~') ||
-	  (field.back() != '~')) {
-	return false;
-      } else {
-	output.assign(field.begin() + 1, field.end() - 1);
-	return true;
-      }
-    };
+    f.close();
 
-    auto parse_mil = [](int& output, const std::string& field) {
-      std::stringstream ss(field);
-      double floating;
-      ss >> floating;
-      if ( ! ss ) {
-	return false;
-      } else {
-	output = lround(floating);
-	return true;
-      }
-    };
-
-    std::string description, amount;
-    int amount_g, kcal, protein_g;
-    if ( remove_tildes(description, descr_field) &&
-	 remove_tildes(amount, amount_field) &&
-	 parse_mil(amount_g, amount_g_field) &&
-	 parse_mil(kcal, kcal_field) &&
-	 parse_mil(protein_g, protein_g_field) ) {
-      result->push_back(std::shared_ptr<Food>(new Food(description,
-						       amount,
-						       amount_g,
-						       kcal,
-						       protein_g)));
-    }
-  }
-
-  f.close();
-
-  return result;
+    return result;
 }
 
 // Convenience function to compute the total kilocalories and protein
 // in a FoodVector. Those values are returned through the
 // first two pass-by-reference arguments.
 void sum_food_vector(int& total_kcal,
-		     int& total_protein_g,
-		     const FoodVector& foods) {
-  total_kcal = total_protein_g = 0;
-  for (auto& food : foods) {
-    total_kcal += food->kcal();
-    total_protein_g += food->protein_g();
-  }
+    int& total_protein_g,
+    const FoodVector& foods) {
+    total_kcal = total_protein_g = 0;
+    for (auto& food : foods) {
+        total_kcal += food->kcal();
+        total_protein_g += food->protein_g();
+    }
 }
 
 // Convenience function to print out each food in a FoodVector,
 // followed by the total kilocalories and protein in it.
 void print_food_vector(const FoodVector& foods) {
-  for (auto& food : foods) {
-    std::cout << food->description()
-	      << " (100 g where each " << food->amount()
-	      << " is " << food->amount_g() << " g)"
-	      << " kcal=" << food->kcal()
-	      << " protein=" << food->protein_g() << " g"
-	      << std::endl;
-  }
-  
-  int total_kcal, total_protein_g;
-  sum_food_vector(total_kcal, total_protein_g, foods);
-  std::cout << "total kcal=" << total_kcal
-	    << " total_protein=" << total_protein_g << " g"
-	    << std::endl;
+    for (auto& food : foods) {
+        std::cout << food->description()
+            << " (100 g where each " << food->amount()
+            << " is " << food->amount_g() << " g)"
+            << " kcal=" << food->kcal()
+            << " protein=" << food->protein_g() << " g"
+            << std::endl;
+    }
+
+    int total_kcal, total_protein_g;
+    sum_food_vector(total_kcal, total_protein_g, foods);
+    std::cout << "total kcal=" << total_kcal
+        << " total_protein=" << total_protein_g << " g"
+        << std::endl;
 }
 
 // Filter the vector source, i.e. create and return a new FoodVector
@@ -187,11 +190,24 @@ void print_food_vector(const FoodVector& foods) {
 // addition, the the vector includes only the first total_size foods
 // that match these criteria.
 std::unique_ptr<FoodVector> filter_food_vector(const FoodVector& source,
-					       int min_kcal,
-					       int max_kcal,
-					       int total_size) {
-  // TODO: implement this function, then delete this comment
-  return nullptr;
+    int min_kcal,
+    int max_kcal,
+    int total_size) {
+
+    int sizeCounter = 0;
+    std::unique_ptr<FoodVector>filteredFoodZeroCal(new FoodVector);
+    
+    for (int i=0;i<total_size;i++)
+    {
+        if (source[i]->kcal()>0 && source[i]->kcal() >= min_kcal && source[i]->kcal() <= max_kcal)
+        {
+            filteredFoodZeroCal->push_back(source[i]);
+            
+        }
+    }
+
+
+    return filteredFoodZeroCal;
 }
 
 // Compute the optimal set of foods with a greedy
@@ -200,9 +216,51 @@ std::unique_ptr<FoodVector> filter_food_vector(const FoodVector& source,
 // greatest. Repeat until no more foods can be chosen, either because
 // we've run out of foods, or run out of calories.
 std::unique_ptr<FoodVector> greedy_max_protein(const FoodVector& foods,
-					       int total_kcal) {
-  // TODO: implement this function, then delete this comment
-  return nullptr;
+    int total_kcal) {
+    FoodVector todo(foods);
+    std::unique_ptr<FoodVector>result(new FoodVector);
+    
+    int result_cal = 0;
+    int result_protein=0;
+    auto f = todo[0]; // creating a temp to that will hold the best food
+    
+    while (!todo.empty())
+
+    {
+        int toDoMaxProtein = 0;
+        int maxPLocation = 0;
+        
+        int c;
+        
+        for (int i = 0;i < todo.size();i++)
+        {
+            if (toDoMaxProtein < todo[i]->protein_g())
+            {
+                toDoMaxProtein = todo[i]->protein_g();
+                f = todo[i]; // assign the best food to the temp
+                maxPLocation = i;
+                c = todo[i]->kcal();
+                
+                
+
+
+            }
+        }
+        todo.erase(todo.begin() + maxPLocation);
+        if (result_cal + c <= total_kcal)
+        {
+            
+            result->push_back(f); 
+            result_cal += c;
+            
+        }
+        if (result_cal >= total_kcal)
+            break;
+
+
+    }
+
+    return result;
 }
 
 // Compute the optimal set of foods with an exhaustive search
@@ -211,9 +269,41 @@ std::unique_ptr<FoodVector> greedy_max_protein(const FoodVector& foods,
 // total protein is greatest. To avoid overflow, the size of the foods
 // vector must be less than 64.
 std::unique_ptr<FoodVector> exhaustive_max_protein(const FoodVector& foods,
-						   int total_kcal) {
-  const int n = foods.size();
-  assert(n < 64);
-  // TODO: implement this function, then delete this comment
-  return nullptr;
+    int total_kcal) {
+    const int n = foods.size();
+    assert(n < 64);
+    // TODO: implement this function, then delete this comment
+    std::unique_ptr<FoodVector>best(new FoodVector);
+    int best_total_cal = 0; 
+    int best_total_prot = 0;
+
+
+    for (uint64_t bits = 0; bits < pow(2, n);bits++)
+    {
+        FoodVector * candidate = new FoodVector;
+        int candidateKCal = 0;
+        int candidateProtein = 0;
+        for (int j = 0;j < n;j++)
+        {
+            if (((bits >> j) & 1) == 1)
+            {
+                candidate->push_back(foods[j]);
+            }
+        
+        }
+        sum_food_vector(candidateKCal, candidateProtein, *candidate);
+        if (candidateKCal <= total_kcal)
+        {
+            if (best->empty() || candidateProtein > best_total_prot)
+            {
+                best = std::unique_ptr<FoodVector>(candidate);
+                sum_food_vector(best_total_cal, best_total_prot, *best);
+            }
+        }
+    }
+    
+
+    return best;
 }
+
+
